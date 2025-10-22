dc54f2b089bfcb629d51720580fa671d91ca4cae
aadarsh16052004-ship-it <aadarsh16052004@gmail.com>
2025-10-22 16:59:36 +0530

    Added dependency for vercel

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from src.templates.app import app as flask_app
-
-# Required by Vercel – wraps Flask app
-def handler(request, *args, **kwargs):
-    return flask_app(request.environ, start_response)
-
-# Expose app variable for Vercel
-app = flask_app
