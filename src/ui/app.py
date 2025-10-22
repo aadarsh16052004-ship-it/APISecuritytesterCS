225746dddad44d27247fe9901316796931b9a640
aadarsh16052004-ship-it <aadarsh16052004@gmail.com>
2025-10-22 19:26:18 +0530

    Update app.py

--- src/ui/app.py
+++ src/ui/app.py
@@ -38,5 +38,7 @@ def run_tests():
 
     return render_template('results.html', stats=checker.stats(), vulns=checker.vulns, recs=checker.recommendations())
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
